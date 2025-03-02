df639d1488a3dfbecf9ec990c901f1a947fa6e87
Pierlee <pierleefranklin@gmail.com>
2025-03-01 20:51:27 -0500

    connected backend with MongoDB database (fixed errors)

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose')
 
 const connectDatabase = () => {
-  mongoose.connect(process.env.DB_LOCAL_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  }).then(con => {
-    console.log(`MongoDB Database connected with HOST ${con.connection.host}`)
+  mongoose.connect(process.env.DB_LOCAL_URI).then(con => {
+    console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
   })
 }
 

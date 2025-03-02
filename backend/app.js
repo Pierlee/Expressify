3acedd03128e641556f302f57a60dbfd00d9c4d7
Pierlee <pierleefranklin@gmail.com>
2025-03-01 20:29:13 -0500

    connected backend with MongoDB database

--- backend/app.js
+++ backend/app.js
@@ -6,5 +6,6 @@ app.use(express.json())
 //Import all routes
 const products = require('./routes/product')
 
+//any route defined in routes/product.js will be prefixed with /api/v1
 app.use('/api/v1', products)
 module.exports = app
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 exports.getProducts = (req, res, next) => {
   res.status(200).json({
     success: true,
-    message: 'This route will show all products in database.'
+    message: 'Fetched products successfully'
   })
 }

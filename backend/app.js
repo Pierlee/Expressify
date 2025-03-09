5ef5931fcae53344682f5ab8807c454c524acce7
Pierlee <pierleefranklin@gmail.com>
2025-03-08 22:06:05 -0500

    Created error handler middleware api and updated error controller

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const errorMiddleware = require('./middlewares/errors')
+
 app.use(express.json())
 
 //Import all routes
@@ -8,4 +10,7 @@ const product = require('./routes/product')
 
 //any route defined in routes/product.js will be prefixed with /api/v1
 app.use('/api/v1', product)
+
+//Middleware to handle errors
+app.use(errorMiddleware)
 module.exports = app
